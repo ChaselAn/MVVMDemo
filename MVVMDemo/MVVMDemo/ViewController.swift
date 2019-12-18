1ea37258535d05947224ac78c7a2a3e0f7b438df
ancheng <cheng.an@duodian.com>
2019-12-18 10:33:58 +0800

    init

---
+++ MVVMDemo/MVVMDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MVVMDemo
+//
+//  Created by ancheng on 2019/12/18.
+//  Copyright © 2019 chaselan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
